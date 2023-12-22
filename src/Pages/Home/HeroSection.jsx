fd5f4e4c17358002d8bee1b7d8f3e5742fe6de58
casrodr <casimiro0623@gmail.com>
2023-12-22 15:23:28 -0800

    Main format done. Need to input information. Webpage is responsive and clickable. Need to fix get in touch button.

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Casimiro Rodriguez</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            This is my portfolio
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/SpillCoffee.gif" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
